e5a5587fd0cd406fa9a511d830c3638719574cb1
peter1599 <peter15@gmail.hu>
2021-10-26 19:38:08 +0200

    Update build.gradle.kts

--- BetterStatus/build.gradle.kts
+++ BetterStatus/build.gradle.kts
@@ -1,11 +1,14 @@
-version = "1.0.6"
+version = "1.0.7"
 description = "BetterStatus replaces default discord status icons"
 
 aliucord {
     changelog.set("""
-        # 1.0.6 Known Issues:
+        # 1.0.7 Known Issues:
         * I know there is resizing bug. Please dont post screenshots about it in support channel or anywhere and dont DM me about it. Im still working on it to fix it.
         * The icons dont rendering on the channel member list. Just scroll down a few and then back up and it will show the icons. I will push an update for this if I figured out how to reredner.
+        * Invisible or Offline users have radial status (sometimes). I know. Fix coming soon.
+        # 1.0.7
+        * Added compatibilty to work with Square Avatars plugin from Juby210#0577
         # 1.0.6
         * A little fix
         # 1.0.5
