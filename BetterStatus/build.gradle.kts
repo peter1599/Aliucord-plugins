a8dbbfef20217f7883d7007c8582a3da3ad1e4a9
peter1599 <peter15@gmail.hu>
2021-10-26 15:11:28 +0200

    Update build.gradle.kts

--- BetterStatus/build.gradle.kts
+++ BetterStatus/build.gradle.kts
@@ -1,12 +1,12 @@
-version = "1.0.5.1"
+version = "1.0.6"
 description = "BetterStatus replaces default discord status icons"
 
 aliucord {
     changelog.set("""
-        # 1.0.5.1 Known Issues:
+        # 1.0.6 Known Issues:
         * I know there is resizing bug. Please dont post screenshots about it in support channel or anywhere and dont DM me about it. Im still working on it to fix it.
         * The icons dont rendering on the channel member list. Just scroll down a few and then back up and it will show the icons. I will push an update for this if I figured out how to reredner.
-        # 1.0.5.1
+        # 1.0.6
         * A little fix
         # 1.0.5
         * Added RadialStatus with 3 options (DM's, ChannelsMemeberList and UserProfile)
