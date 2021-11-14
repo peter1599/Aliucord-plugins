e3cea8debfcd6e8468c902eaec2cd692b2cb16f8
peter1599 <peter15@gmail.hu>
2021-11-14 17:25:23 +0100

    Update build.gradle.kts

--- BetterStatus/build.gradle.kts
+++ BetterStatus/build.gradle.kts
@@ -1,8 +1,17 @@
-version = "1.0.7"
+version = "1.0.8"
 description = "BetterStatus replaces default discord status icons"
 
 aliucord {
     changelog.set("""
+        # 1.0.8 Known Issues:
+        * Own avatar still gets radial status
+        * Resizing bug still exists
+        * ChannelMembersList sometimes shows "wrong" status icon(s) and the icons dont rendering correctly. Just scroll down a few and then back up or vice-versa and it will show the icons.
+        # 1.0.8:
+        * Optimizations and bug fixes. (Huge thanks for mantikafasi#4444, ID: 287555395151593473)
+        * Fixed offline users had radial status (mantikafasi#4444)
+        * Added a new option toggle, Chat Status: Displays a little status circle next to usernames.
+        * Added a new option toggle, RadialStatus (Chat): Shows a status ring around the user avatar in the Chat.
         # 1.0.7 Known Issues:
         * I know there is resizing bug. Please dont post screenshots about it in support channel or anywhere and dont DM me about it. Im still working on it to fix it.
         * The icons dont rendering on the channel member list. Just scroll down a few and then back up and it will show the icons. I will push an update for this if I figured out how to reredner.
